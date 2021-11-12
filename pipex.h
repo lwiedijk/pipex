b83c25df48ee7f0f42760afb7deead2306810a7d
Laura Wiedijk <67950116+lwiedijk@users.noreply.github.com>
2021-11-12 09:32:15 +0100

    Add files via upload

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: lwiedijk <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/03 11:07:46 by lwiedijk      #+#    #+#                 */
-/*   Updated: 2021/11/03 11:16:36 by lwiedijk      ########   odam.nl         */
+/*   Updated: 2021/11/12 08:54:38 by lwiedijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,30 @@
 # define PIPEX_H
 
 # define MODE_RW_R_R 0644
+# define TRUE 1
+# define FALSE 0
+
+typedef enum e_error_status
+{
+	OK,
+	ERROR,
+	SYS_CALL_ERR,
+	USAGE,
+	MALLOC_FAIL,
+}	t_error_status;
+
+typedef struct s_exec_vectors
+{
+	char	**vector1;
+	char	**vector2;
+}					t_exec_vectors;
+
+typedef struct s_all_fd
+{
+	int	fd_in;
+	int	fd_out;
+	int	pipe_end[2];
+}					t_all_fd;
 
 
-
-
-#endif 
+#endif
