4785e06af3a4849b07b8c0c642d2971401196dd9
Laura Wiedijk <67950116+lwiedijk@users.noreply.github.com>
2021-11-17 13:00:08 +0100

    waitpid print

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: lwiedijk <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/20 11:04:06 by lwiedijk      #+#    #+#                 */
-/*   Updated: 2021/11/17 09:56:26 by lwiedijk      ########   odam.nl         */
+/*   Updated: 2021/11/17 12:00:13 by lwiedijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,6 +67,8 @@ void	fork_processes(t_exec_vectors *exec_vectors,
 {
 	pid_t	pid_1;
 	pid_t	pid_2;
+	pid_t	wait_return1;
+	pid_t	wait_return2;
 	int		status;
 
 	status = 5;
@@ -75,15 +77,19 @@ void	fork_processes(t_exec_vectors *exec_vectors,
 		error_and_exit(SYS_CALL_ERR, exec_vectors);
 	if (pid_1 == 0)
 		child1(envp, all_fd, exec_vectors);
-	waitpid(-1, &status, 0);
-	printf("status = [%d]\n", status);
-	printf("exitstatus = [%d]\n", WEXITSTATUS(status));
 	pid_2 = fork();
 	if (pid_2 == -1)
 		error_and_exit(SYS_CALL_ERR, exec_vectors);
 	if (pid_2 == 0)
 		child2(envp, all_fd, exec_vectors);
-	waitpid(-1, &status, 0);
+	wait_return1 = waitpid(pid_1, &status, 0);
 	printf("status = [%d]\n", status);
 	printf("exitstatus = [%d]\n", WEXITSTATUS(status));
+	printf("wait_return1 = [%d]\n", wait_return1);
+	printf("child1_pid = [%d]\n", pid_1);
+	wait_return2 = waitpid(pid_2, &status, 0);
+	printf("status = [%d]\n", status);
+	printf("exitstatus = [%d]\n", WEXITSTATUS(status));
+	printf("wait_return2 = [%d]\n", wait_return2);
+	printf("child2_pid = [%d]\n", pid_2);
 }
