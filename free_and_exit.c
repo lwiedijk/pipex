c8dd4224a6c1e4879cd710b67aa52721d7ee6ea4
Laura frouke Wiedijk <lwiedijk@f0r6s2.codam.nl>
2022-01-25 07:53:09 +0100

    add comment

--- free_and_exit.c
+++ free_and_exit.c
@@ -6,7 +6,7 @@
 /*   By: lwiedijk <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/12 11:40:28 by lwiedijk      #+#    #+#                 */
-/*   Updated: 2022/01/19 11:57:33 by lwiedijk      ########   odam.nl         */
+/*   Updated: 2022/01/25 07:52:31 by lwiedijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,7 @@ void	free_3d_array(char ***cmd_array)
 		i++;
 	}
 	free(cmd_array);
+	//git try
 }
 
 void	free_on_error(char ***cmd_vectors)
